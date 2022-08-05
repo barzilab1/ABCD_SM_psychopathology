e6fb14bf93bdc5b1ed26e0c0ebc815a3d72b67e7
trant13@chop.edu <trant13@chop.edu>
2022-08-05 13:43:53 -0400

    Clean coldes.

--- scripts/merging.R
+++ scripts/merging.R
@@ -14,10 +14,8 @@ physicalhealth_sum <- read_csv("outputs/physicalhealth_sum.csv")
 psychopathology <- read_csv("outputs/psychopathology.csv")
 psychopathology_sum <- read_csv("outputs/psychopathology_sum_scores.csv")
 suicide_set <- read_csv("outputs/suicide_set.csv")
-
 demographics_baseline <- read.csv("outputs/demographics_baseline.csv")
 demographics_long <- read.csv("outputs/demographics_long.csv")
-
 
 # combine the demographics to one dataset
 demo_race = demographics_baseline[,grep("src|race|hisp", colnames(demographics_baseline))]
@@ -39,38 +37,42 @@ dataset <- merge(dataset, demographics, all =T )
 
 dataset <- dataset[!dataset$eventname %in% c("3_year_follow_up_y_arm_1"),]
 
-
-geo_data_baseline = geo_data[geo_data$eventname == "baseline_year_1_arm_1", grep("src|reshist_state_[^m]|reshist_addr1_adi_perc", colnames(geo_data), value = T)]
+geo_data_baseline <- geo_data[geo_data$eventname == "baseline_year_1_arm_1", grep("src|reshist_state_[^m]|reshist_addr1_adi_perc", colnames(geo_data), value = T)]
 
 dataset <- merge(dataset, family[,c("src_subject_id", "sex", "rel_family_id")], all.x = T)
 dataset <- merge(dataset, geo_data_baseline)
 
+dataset <- dataset %>% 
+  mutate(eventname = recode(eventname, 
+                            `1_year_follow_up_y_arm_1` = "1",
+                            `2_year_follow_up_y_arm_1` = "2",
+                            baseline_year_1_arm_1 = "bl"),
+         age_year = age/12) 
 
 write.csv(file = "outputs/dataset_SGM_3tp.csv", x = dataset, row.names = F, na = "")
 
-
 # Create data for mixed models (at 1-year and 2-year follow-up)
-dataset_long <- dataset[dataset$eventname %in% c("1_year_follow_up_y_arm_1","2_year_follow_up_y_arm_1"),]
+dataset_long <- dataset[dataset$eventname %in% c("1","2"),]
 dataset_long <- dataset_long[,colSums(is.na(dataset_long)) != nrow(dataset_long)]
-dataset_long <- dataset_long %>% 
-  mutate(eventname = recode(eventname, 
-                            `1_year_follow_up_y_arm_1` = "1",
-                            `2_year_follow_up_y_arm_1` = "2"),
-         age_year = age/12)
+# dataset_long <- dataset_long %>% 
+#   mutate(eventname = recode(eventname, 
+#                             `1_year_follow_up_y_arm_1` = "1",
+#                             `2_year_follow_up_y_arm_1` = "2"),
+#          age_year = age/12)
 
-dataset_long$SI_y <- as.factor(dataset_long$SI_y)
-dataset_long$SA_y <- as.factor(dataset_long$SA_y)
+# dataset_long$SI_y <- as.factor(dataset_long$SI_y)
+# dataset_long$SA_y <- as.factor(dataset_long$SA_y)
 
 # replace parent education - 2y as 1y
 edu_2y <- dataset_long %>% filter(eventname == "1") %>% 
   dplyr::select(src_subject_id, parents_avg_edu) %>% 
-  rename(parents_avg_edu_2y = "parents_avg_edu") %>% 
+  rename(parents_avg_edu_2 = "parents_avg_edu") %>% 
   mutate(eventname = "2")
 
 dataset_long <- dataset_long %>% 
   left_join(edu_2y) %>% 
-  mutate(parents_avg_edu = coalesce(parents_avg_edu, parents_avg_edu_2y)) %>% 
-  dplyr::select(-parents_avg_edu_2y)
+  mutate(parents_avg_edu = coalesce(parents_avg_edu, parents_avg_edu_2)) %>% 
+  dplyr::select(-parents_avg_edu_2)
 
 # Regressed out ADI from household income # Create residuals of househ old income
 dataset_long <- add_residuals(data = dataset_long,
